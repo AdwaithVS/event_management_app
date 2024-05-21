86581f6bb3723a8167b43809ebbb9202abe5d9f2
AdwaithVS <adwaithvs512@gmail.com>
2024-05-21 22:36:49 +0530

    Service page

--- src/components/ServicesPage.jsx
+++ src/components/ServicesPage.jsx
@@ -6,72 +6,110 @@ const ServicesPage = () => {
             <div className="container">
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                        <div class="card">
-                            <div class="card-body text-center">
-                                <h3>Unlock Unforgettable Experiences</h3>
-                                <p>Our comprehensive services are meticulously designed to cater to every aspect of your event, ensuring every moment is perfect. From seamless planning and coordination to exquisite details and impeccable execution, we're dedicated to crafting memorable experiences that exceed your expectations and leave a lasting impression on you and your guests.</p>
-                            </div>
-                        </div>
                         <div className="row g-3">
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                <div class="card">
+                                    <div class="card-body text-center">
+                                        <h3 class="card-text">Unlock Unforgettable Experiences</h3>
+                                        <p class="card-text">Our comprehensive services are meticulously designed to cater to every aspect of your event, ensuring every moment is perfect. From seamless planning and coordination to exquisite details and impeccable execution, we're dedicated to crafting memorable experiences that exceed your expectations and leave a lasting impression on you and your guests.</p>
+                                    </div>
+                                </div>
+                            </div>
                             <div className="col col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
                                 <div class="card">
                                     <img src="https://media.istockphoto.com/id/1186214696/photo/hindu-wedding-ritual-wherein-bride-and-groom-hand.jpg?s=612x612&w=0&k=20&c=fTlNejRdY7dkvk742auNgI3j6Ve9UqqWSnb3QJ-D2gw=" class="card-img-top" alt="..." />
                                     <div class="card-body">
                                         <h4 class="card-text">Wedding Ceremony</h4>
-                                        <p class="card-text">Our wedding ceremony services cover everything.</p>
-                                        <ul>
-                                            <li>Venue selection</li>
-                                            <li>Seamless planning</li>
-                                            <li>Vendor coordination</li>
-                                            <li>Personalized decor</li>
-                                            <li>Guest management</li>
-                                            <li>On-the-day assistance</li>
-                                            <li>Photography</li>
-                                            <li>Videography</li>
-                                            <li>Post-event support</li>
-                                            <li>Honeymoon packages</li>
-                                        </ul>
-                                        <p class="card-text">We ensure your special day is stress-free and unforgettable.</p>
+                                        <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos, numquam. Incidunt, maiores? In facilis accusantium vero ducimus totam perspiciatis iste reiciendis, consequuntur aspernatur. Beatae magnam facilis itaque harum eum! Ratione!
+                                            Natus qui incidunt nihil cupiditate praesentium distinctio eos atque ab voluptates ut aliquid aperiam perspiciatis, obcaecati architecto consequatur est reiciendis in dolorum ex quidem magnam. Assumenda voluptate quos molestias fugit.</p>
                                     </div>
                                 </div>
                             </div>
                             <div className="col col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
                                 <div class="card">
-                                    <img src="..." class="card-img-top" alt="..." />
+                                    <img src="https://images.unsplash.com/photo-1625649611137-df49dc542f6a?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="card-img-top" alt="..." />
                                     <div class="card-body">
-                                        <p class="card-text"></p>
+                                        <h4 class="card-text">Birthday Party</h4>
+                                        <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos, numquam. Incidunt, maiores? In facilis accusantium vero ducimus totam perspiciatis iste reiciendis, consequuntur aspernatur. Beatae magnam facilis itaque harum eum! Ratione!
+                                            Natus qui incidunt nihil cupiditate praesentium distinctio eos atque ab voluptates ut aliquid aperiam perspiciatis, obcaecati architecto consequatur est reiciendis in dolorum ex quidem magnam. Assumenda voluptate quos molestias fugit.</p>
                                     </div>
                                 </div>
                             </div>
                             <div className="col col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
                                 <div class="card">
-                                    <img src="..." class="card-img-top" alt="..." />
+                                    <img src="https://5.imimg.com/data5/TI/LQ/MY-19128035/corporate-events.jpg" class="card-img-top" alt="..." />
                                     <div class="card-body">
-                                        <p class="card-text"></p>
+                                        <h4 class="card-text">Corporate Event</h4>
+                                        <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos, numquam. Incidunt, maiores? In facilis accusantium vero ducimus totam perspiciatis iste reiciendis, consequuntur aspernatur. Beatae magnam facilis itaque harum eum! Ratione!
+                                            Natus qui incidunt nihil cupiditate praesentium distinctio eos atque ab voluptates ut aliquid aperiam perspiciatis, obcaecati architecto consequatur est reiciendis in dolorum ex quidem magnam. Assumenda voluptate quos molestias fugit.</p>
                                     </div>
                                 </div>
                             </div>
                             <div className="col col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
                                 <div class="card">
-                                    <img src="..." class="card-img-top" alt="..." />
+                                    <img src="https://images.unsplash.com/photo-1530541930197-ff16ac917b0e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="card-img-top" alt="..." />
                                     <div class="card-body">
-                                        <p class="card-text"></p>
+                                        <h4 class="card-text">Camps & Outdoor Retreats</h4>
+                                        <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos, numquam. Incidunt, maiores? In facilis accusantium vero ducimus totam perspiciatis iste reiciendis, consequuntur aspernatur. Beatae magnam facilis itaque harum eum! Ratione!
+                                            Natus qui incidunt nihil cupiditate praesentium distinctio eos atque ab voluptates ut aliquid aperiam perspiciatis, obcaecati architecto consequatur est reiciendis in dolorum ex quidem magnam. Assumenda voluptate quos molestias fugit.</p>
                                     </div>
                                 </div>
                             </div>
                             <div className="col col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
                                 <div class="card">
-                                    <img src="..." class="card-img-top" alt="..." />
+                                    <img src="https://images.unsplash.com/photo-1524368535928-5b5e00ddc76b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="card-img-top" alt="..." />
                                     <div class="card-body">
-                                        <p class="card-text"></p>
+                                        <h4 class="card-text">Music Concert</h4>
+                                        <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos, numquam. Incidunt, maiores? In facilis accusantium vero ducimus totam perspiciatis iste reiciendis, consequuntur aspernatur. Beatae magnam facilis itaque harum eum! Ratione!
+                                            Natus qui incidunt nihil cupiditate praesentium distinctio eos atque ab voluptates ut aliquid aperiam perspiciatis, obcaecati architecto consequatur est reiciendis in dolorum ex quidem magnam. Assumenda voluptate quos molestias fugit.</p>
                                     </div>
                                 </div>
                             </div>
                             <div className="col col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
                                 <div class="card">
-                                    <img src="..." class="card-img-top" alt="..." />
+                                    <img src="https://images.unsplash.com/photo-1503095396549-807759245b35?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="card-img-top" alt="..." />
                                     <div class="card-body">
-                                        <p class="card-text"></p>
+                                        <h4 class="card-text">Theatre Performance</h4>
+                                        <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos, numquam. Incidunt, maiores? In facilis accusantium vero ducimus totam perspiciatis iste reiciendis, consequuntur aspernatur. Beatae magnam facilis itaque harum eum! Ratione!
+                                            Natus qui incidunt nihil cupiditate praesentium distinctio eos atque ab voluptates ut aliquid aperiam perspiciatis, obcaecati architecto consequatur est reiciendis in dolorum ex quidem magnam. Assumenda voluptate quos molestias fugit.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                                <div class="card">
+                                    <img src="https://images.unsplash.com/photo-1608471028014-47037ab2c3ad?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="card-img-top" alt="..." />
+                                    <div class="card-body">
+                                        <h4 class="card-text">Engagement Party</h4>
+                                        <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos, numquam. Incidunt, maiores? In facilis accusantium vero ducimus totam perspiciatis iste reiciendis, consequuntur aspernatur. Beatae magnam facilis itaque harum eum! Ratione!
+                                            Natus qui incidunt nihil cupiditate praesentium distinctio eos atque ab voluptates ut aliquid aperiam perspiciatis, obcaecati architecto consequatur est reiciendis in dolorum ex quidem magnam. Assumenda voluptate quos molestias fugit.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                                <div class="card">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXTRaptlh8ZanOu6Gz1_A-zTAowJG6QKPgGSVC0I7CIg&s" class="card-img-top" alt="..." />
+                                    <div class="card-body">
+                                        <h4 class="card-text">Holiday Party</h4>
+                                        <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos, numquam. Incidunt, maiores? In facilis accusantium vero ducimus totam perspiciatis iste reiciendis, consequuntur aspernatur. Beatae magnam facilis itaque harum eum! Ratione!
+                                            Natus qui incidunt nihil cupiditate praesentium distinctio eos atque ab voluptates ut aliquid aperiam perspiciatis, obcaecati architecto consequatur est reiciendis in dolorum ex quidem magnam. Assumenda voluptate quos molestias fugit.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                                <div class="card">
+                                    <img src="https://media.istockphoto.com/id/1381030718/photo/barsana-holi-one-of-the-most-joyful-festival-of-india-this-is-birth-place-of-radha-lord.jpg?s=612x612&w=0&k=20&c=c0kcjHpSFJXg7F4D6s8Ez-7RWY3MjoIrwsiRQKScank=" class="card-img-top" alt="..." />
+                                    <div class="card-body">
+                                        <h4 class="card-text">Festival Celebration Party</h4>
+                                        <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos, numquam. Incidunt, maiores? In facilis accusantium vero ducimus totam perspiciatis iste reiciendis, consequuntur aspernatur. Beatae magnam facilis itaque harum eum! Ratione!
+                                            Natus qui incidunt nihil cupiditate praesentium distinctio eos atque ab voluptates ut aliquid aperiam perspiciatis, obcaecati architecto consequatur est reiciendis in dolorum ex quidem magnam. Assumenda voluptate quos molestias fugit.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                <div class="card">
+                                    <div class="card-body text-center">
+                                        <h3 class="card-text">Book <b>YOUR</b> event today!</h3>
+                                        <a className="btn btn-primary" href="#">Click Here!</a>
                                     </div>
                                 </div>
                             </div>
